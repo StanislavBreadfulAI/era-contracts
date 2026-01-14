347e1c6cd33f6b11f57665839232f672e6f8f6f8
Anton Baliasnikov <aba@matterlabs.dev>
2026-01-14 16:10:10 +0000

    chore: update hashes

--- recompute_hashes.sh
+++ recompute_hashes.sh
@@ -4,12 +4,12 @@
 set -e
 
 # Expected Foundry version and commit
-EXPECTED_VERSION="forge Version: 1.3.5-foundry-zksync-v0.0.30"
-EXPECTED_COMMIT="b9f3efc17"
+EXPECTED_VERSION="forge Version: 1.3.5-foundry-zksync-v0.1.4"
+EXPECTED_COMMIT="932f78107"
 
 # Check if Foundry is installed
 if ! command -V forge &> /dev/null; then
-  echo "Foundry is not installed. Please install it using \"foundryup-zksync -i 0.0.30\"."
+  echo "Foundry is not installed. Please install it using \"foundryup-zksync -i 0.1.4\"."
   exit 1
 fi
 
@@ -22,7 +22,7 @@ if [[ "$FORGE_VERSION" != "$EXPECTED_VERSION" ]]; then
   echo "Incorrect Foundry version."
   echo "Expected: ${EXPECTED_VERSION}"
   echo "Found:    ${FORGE_VERSION}"
-  echo "Run: foundryup-zksync -i 0.0.30"
+  echo "Run: foundryup-zksync -i 0.1.4"
   exit 1
 fi
 
